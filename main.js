de02266c78228903920a3c3977077de4b9d2680e
SF4NU <mariusmarius200121@gmail.com>
2024-02-20 22:01:39 +0100

    finito la parte di css

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-const form = document.querySelector('.second-form');
+const form = document.querySelector(".second-form");
 const goToLogin = document.querySelector(".goToLogin");
 let isTrue = false;
 
-document.querySelector('.submit-button').addEventListener('click', () => {
+document.querySelector(".submit-button").addEventListener("click", () => {
   if (!isTrue) {
     form.classList.add("move-form");
   }
-})
+});
 
-goToLogin.addEventListener('click', () => {
+goToLogin.addEventListener("click", () => {
   if (!isTrue) {
-    form.classList.remove("move-form-back");
+    form.classList.remove("move-form2");
     form.classList.add("move-form");
-    goToLogin.style.display = "none";
+    goToLogin.textContent = "To Sign-Up";
     isTrue = true;
-  }
-  else {
+  } else {
     isTrue = false;
     form.classList.remove("move-form");
-    form.classList.add("move-form-back");
+    form.classList.add("move-form2");
   }
-})
+});
